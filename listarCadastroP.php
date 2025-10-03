687af4832e1b3fb120a66d5a0585eba2674b6444
Veldrin <allangui619@gmail.com>
2025-10-02 22:41:39 -0300

    adicionando novos campos

---
+++ listarCadastroP.php
@@ -0,0 +1,18 @@
+<h1>Lista de Pacientes</h1>
+<div class="row">
+
+<?php
+
+    include 'conexao.php';
+    $consultas = $conexao->query('select * from consulta');
+
+    while($consulta = $consultas->fetch_object())
+    echo "<div class='card m-2' style='width: 18rem;'>
+        <img src='https://cdn-icons-png.flaticon.com/512/12187/12187259.png' class='card-img-top' alt='...'>
+        <div class='card-body'>
+        <h5 class='card-text'>$consulta->paciente</h5>
+        <a href='delete.php?numero=$consulta->paciente' class='btn btn-danger'>Deletar</a>
+        </div>
+    </div>"
+
+?>
